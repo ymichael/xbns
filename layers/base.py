8fae4ecb5a9205d88ffdbf3f31efd74d394ff476
Michael Yong <wrong92@gmail.com>
2014-10-26 16:27:08 +0800

    Update layers/base.py
    - Add is empty methods
    - Add comments to disambiguate between incoming and outgoing queues.

--- layers/base.py
+++ layers/base.py
@@ -4,13 +4,37 @@ import Queue as queue
 class BaseLayer(object):
     """Base class for each layer."""
     def __init__(self):
+        # From this layer to a higher layer.
         self.incoming_queue = queue.Queue()
+        # From this layer to a lower layer.
         self.outgoing_queue = queue.Queue()
 
+    def is_incoming_empty(self):
+        """Returns whether the incoming queue is empty.
+
+        Whether there is any incoming data buffered for the next (higher) layer.
+        """
+        return self.incoming_queue.empty()
+
+    def is_outgoing_empty(self):
+        """Returns whether the outgoing queue is empty.
+
+        Whether there is any outgoing data buffered for the next (lower) layer.
+        """
+        return self.outgoing_queue.empty()
+
     def get_incoming(self):
+        """Returns the next item in the incoming queue.
+
+        Blocks if queue is empty.
+        """
         return self.incoming_queue.get()
 
     def get_outgoing(self):
+        """Returns the next item in the outgoing queue.
+
+        Blocks if queue is empty.
+        """
         return self.outgoing_queue.get()
 
     def process_incoming(self, data):
