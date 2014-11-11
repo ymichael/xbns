66b5ee361b775d76db64ded46c9c2f81df215f0d
Michael Yong <wrong92@gmail.com>
2014-11-11 19:32:13 +0800

    Truncate sent message log.

--- net/layers/application.py
+++ net/layers/application.py
@@ -22,7 +22,7 @@ class Application(base.BaseLayer):
 
         self.process_outgoing(data, metadata)
         self.logger.debug("(%s, %s): Sent: %s" % \
-            (self.addr, self.get_port(), data))
+            (self.addr, self.get_port(), data[:10] + "..."))
 
     def start_incoming(self, incoming_layer):
         while True:
