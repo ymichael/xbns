38f54cfbb6f4a2eba55824facfa2db4eede0fb47
Michael Yong <wrong92@gmail.com>
2015-01-13 14:50:07 +0800

    v1 Deluge protocol.

--- app/deluge.py
+++ app/deluge.py
@@ -80,38 +80,37 @@ class DelugePDU(object):
         return cls(cls.REQ, message)
 
 
+class State(object):
+    MAINTAIN = 1
+    RX = 2
+    TX = 3
+
+
 class Deluge(net.layers.application.Application):
     PORT = 11
     PAGE_SIZE = 1000
     PACKET_SIZE = 100
 
-    def __init__(self):
-        super(Deluge, self).__init__()
-        self.current_version = 0
-        self.current_pages = {}
-        self.packets_per_page = []
-        self.buffering_pages = {}
+    T_MIN = .1
+    T_MAX = 5
+    K = 1
 
-        self.should_adv = 1
-        self.requested = set()
+    TX_T = .1
 
     def get_port(self):
         return self.PORT
 
-    def new_version(self, version, data=None):
-        if version <= self.current_version:
+    def new_version(self, version, data):
+        if version <= self.version:
             return
 
-        self.current_version = version
-        self.current_pages = {}
+        self.version = version
+        self.data = data
+        self.complete_pages = {}
         self.packets_per_page =[]
         self.buffering_pages = {}
 
-        if data is not None:
-            self.split_and_set_pages(data)
-            self.send_adv()
-
-    def split_and_set_pages(self, data):
+        # TODO: Make this cleaner.
         i = 0
         page_number = 0
         while i < len(data):
@@ -123,55 +122,249 @@ class Deluge(net.layers.application.Application):
                 packets[packet_number] = page[j:j + self.PACKET_SIZE]
                 packet_number += 1
                 j += self.PACKET_SIZE
-            self.current_pages[page_number] = packets
+            self.complete_pages[page_number] = packets
             self.packets_per_page.append(len(packets))
             page_number += 1
             i += self.PAGE_SIZE
 
-    def send_adv(self):
-        adv = DelugePDU.create_adv_packet(
-            self.current_version, self.current_pages.keys(),
-            self.packets_per_page)
-        self.send(adv.to_string())
+    def __init__(self):
+        super(Deluge, self).__init__()
 
-    def send_req(self, page_number, packets):
-        req = DelugePDU.create_req_packet(
-            self.current_version, page_number, packets)
-        self.send(req.to_string())
+        # The current version
+        self.version = 0
 
-    def send_data(self, page_number, packet_number):
-        data = DelugePDU.create_data_packet(
-            self.current_version, page_number, packet_number,
-            self.current_pages[page_number][page_number])
-        self.send(data.to_string())
+        # The state of the protocols. Starts in the MAINTAIN state.
+        self.state = State.MAINTAIN
+
+        # The length of the window. This is dynamically adjusted to be between
+        # T_MIN and T_MAX to allow for fast propagation of new versions and low
+        # maintainance overhead.
+        self.t = self.T_MIN
+
+        # Number of ADV overheard that are similar to self during the current
+        # window.
+        self.k = 0
+
+        # Whether network is inconsistent. This results when we overhear an ADV
+        # that does not match our state (both ahead and behind).
+        # This is reset per window and used to dynamically adjust the window
+        # length.
+        self.inconsistent = False
+
+        # A buffer of DATA (page, packet) tuples to send. We remove from this
+        # set if we overhear a similar DATA (page, packet).
+        self._pending_datas = set()
+
+        # A buffer of REQ, page numbers, to send. We remove from this
+        # set if we overhear a similar REQ (page, packets).
+        self._pending_reqs = set()
+
+        # Page/Packet information.
+        self.data = ""
+        self.complete_pages = {}
+        self.packets_per_page = []
+        self.buffering_pages = {}
+
+        # Timers and threads
+        self._send_adv_timer = None
+        self._next_round_timer = None
+
 
     def start(self, *args, **kwargs):
         super(Deluge, self).start(*args, **kwargs)
-        # Start thread to send ADV.
-        t1 = threading.Thread(target=self.maybe_send_adv)
-        t1.setDaemon(True)
-        t1.start()
 
-    def maybe_send_adv(self):
-        # Only send adv if have
-        while True:
-            if len(self.current_pages) != 0:
-                if self.should_adv == 1:
-                    self.send_adv()
-                else:
-                    self.should_adv += 1
-            time.sleep(random.random())
+        # Start round.
+        self._restart_round()
 
-    def delayed_send_requested(self):
-        time.sleep(1)
-        for page, packet in self.requested:
-            self.send_data(page, packet)
-        self.requested = set()
+    def _round(self):
+        if self.state == State.MAINTAIN:
+            self._round_maintain()
+        elif self.state == State.RX:
+            self._round_rx()
+        elif self.state == State.TX:
+            self._round_tx()
 
-    def send_requested(self):
-        t1 = threading.Thread(target=self.delayed_send_requested)
-        t1.setDaemon(True)
-        t1.start()
+        # Start timer for next round.
+        self._next_round_timer.cancel()
+        self._next_round_timer = threading.Timer(self.t, self._round)
+        self._next_round_timer.start()
+
+    def _round_maintain(self):
+        # Reset k to 0
+        self.k = 0
+
+        # If no overhead packet indicates an inconsistency in the previous
+        # round, update t
+        if not self.inconsistent:
+            self.t = min(self.t * 2, self.T_MAX)
+        self.inconsistent = False
+
+        # Wait for a random amount of time (between self.t / 2 and self.t)
+        rand_t = self._get_random_t_adv()
+        self._send_adv_timer = threading.Timer(rand_t, self._send_adv)
+        self._send_adv_timer.start()
+
+    def _round_rx(self):
+        rand_req_t = self._get_random_t_req()
+        time.sleep(rand_req_t)
+        if len(self._pending_reqs) != 0:
+            req_page = self._pending_reqs.pop()
+            all_packets_in_page = set(xrange(self.packets_per_page[req_page]))
+            if req_page in self.buffering_pages:
+                received_packets = self.buffering_pages[req_page].keys()
+                missing_packets = all_packets_in_page - set(received_packets)
+            else:
+                missing_packets = all_packets_in_page
+            self.send_req(req_page, missing_packets)
+
+        # Go back to the MAINTAIN state.
+        self.state = State.MAINTAIN
+
+    def _round_tx(self):
+        while len(self._pending_datas) != 0:
+            rand_tx_t = self._get_random_t_tx()
+            time.sleep(rand_tx_t)
+            if len(self._pending_datas) != 0:
+                page, packet = self._pending_datas.pop()
+                self.send_data(page, packet)
+
+        # Go back to the MAINTAIN state.
+        self.state = State.MAINTAIN
+
+    def _get_random_t_adv(self):
+        return random.uniform(self.t / 2.0, self.t)
+
+    def _get_random_t_req(self):
+        return random.uniform(0, self.t / 2.0)
+
+    def _get_random_t_tx(self):
+        return random.uniform(0, self.TX_T)
+
+    def _send_adv(self):
+        # Only send ADV if during the current window, we overhear less than K
+        # summaries with similar (v, pages).
+        if self.k >= self.K:
+            return
+
+        # TMP:
+        if len(self.complete_pages.keys()) == 0:
+            return
+
+        adv = DelugePDU.create_adv_packet(
+            self.version, self.complete_pages.keys(), self.packets_per_page)
+        self.send(adv.to_string())
+
+    def _process_adv(self, data_unit):
+        if self.state == State.MAINTAIN:
+            # Check if network is consistent.
+            if data_unit.version > self.version:
+                self.version = data_unit.version
+                self.packets_per_page = data_unit.packets_per_page
+                self.buffering_pages = {}
+                self.complete_pages = {}
+
+            adv_pages = set(data_unit.pages)
+            own_pages = set(self.complete_pages.keys())
+
+            # Increase k if ADV is same.
+            if adv_pages == own_pages:
+                self.k += 1
+            else:
+                missing_pages = adv_pages - own_pages
+                if len(missing_pages) != 0 or \
+                        max(adv_pages) > max(own_pages):
+                    self.state = State.RX
+
+                    # Queue the various packets to request.
+                    req_page = min(missing_pages)
+                    self._pending_reqs.add(req_page)
+
+                # Set t to T_MIN and start new round.
+                self.inconsistent = True
+                self.t = self.T_MIN
+                self._restart_round()
+
+    def _restart_round(self):
+        # NOTE: Do not cancel send_adv timer here. The adv only gets cancelled
+        # if more than k of the same adv are overheard during the specified
+        # time.
+        if self._next_round_timer is not None:
+            self._next_round_timer.cancel()
+
+        self._next_round_timer = threading.Timer(0, self._round)
+        self._next_round_timer.start()
+
+    def process_incoming(self, data, metadata=None):
+        data_unit = DelugePDU.from_string(data)
+        self.log(data_unit, metadata)
+        if data_unit.is_adv():
+            self._process_adv(data_unit)
+        elif data_unit.is_req():
+            self._process_req(data_unit)
+        elif data_unit.is_data():
+            self._process_data(data_unit)
+
+    def _process_req(self, data_unit):
+        if self.version != data_unit.version:
+            return
+
+        # Remove similar REQ from pending buffer, so that we don't flood the
+        # network with a similar, useless packet.
+        if data_unit.page_number in self._pending_reqs:
+            self._pending_reqs.remove(data_unit.page_number)
+
+        # React to REQ, transit to TX state if we have the requested page.
+        if self.state == State.MAINTAIN:
+            if data_unit.page_number in self.complete_pages:
+                self.state = State.TX
+
+        # Add to data packets to queue.
+        if self.state == State.MAINTAIN or self.state == State.TX:
+            for packet in data_unit.packets:
+                self._pending_datas.add((data_unit.page_number, packet))
+
+    def _process_data(self, data_unit):
+        if self.version != data_unit.version:
+            return
+
+        # Remove similar DATA from pending buffer, so that we don't flood the
+        # network with a similar, useless packet.
+        # TODO: A more aggressive strategy would remove all DATA packets from
+        # the same overheard page.
+        page_no = data_unit.page_number
+        packet_no = data_unit.packet_number
+        data = (page_no, packet_no)
+        if data in self._pending_datas:
+            self._pending_datas.remove(data)
+
+        # Store data if applicable.
+        if page_no not in self.buffering_pages:
+            self.buffering_pages[page_no] = {}
+        if packet_no not in self.buffering_pages[page_no]:
+            self.buffering_pages[page_no][packet_no] = data_unit.message
+
+        # If we complete a page, move it to complete page.
+        if len(self.buffering_pages[page_no]) == self.packets_per_page[page_no]:
+            # If we are in the RX state, and the completed page is the page
+            # that triggered entry into the RX state, transit to the MAINTAIN
+            # state. (TODO).
+            if self.state == State.RX:
+                self.state = State.MAINTAIN
+            self.complete_pages[page_no] = self.buffering_pages[page_no]
+            del self.buffering_pages[page_no]
+
+    def send_data(self, page_number, packet_number):
+        try:
+            data = DelugePDU.create_data_packet(
+                self.version, page_number, packet_number,
+                self.complete_pages[page_number][packet_number])
+            self.send(data.to_string())
+        except KeyError:
+            pass
+
+    def send_req(self, page_number, packets):
+        req = DelugePDU.create_req_packet(self.version, page_number, packets)
+        self.send(req.to_string())
 
     def log(self, data_unit, metadata):
         # tmp. figure out something cleaner.
@@ -185,81 +378,9 @@ class Deluge(net.layers.application.Application):
             (self.addr, self.get_port(), data_unit.get_msg_type(), msg,
                 metadata.sender_addr))
 
-    def process_incoming(self, data, metadata=None):
-        data_unit = DelugePDU.from_string(data)
-        self.log(data_unit, metadata)
-        if data_unit.is_adv():
-            self.process_adv(data_unit)
-        elif data_unit.is_req():
-            self.process_req(data_unit)
-        elif data_unit.is_data():
-            self.process_data(data_unit)
-
-    def suppress_adv(self):
-        self.should_adv -= 1
-
-    def process_adv(self, data_unit):
-        # Maybe update version number.
-        self.new_version(data_unit.version)
-        self.packets_per_page = data_unit.packets_per_page
-
-        # Ignore if we already have all pages.
-        adv_pages = set(data_unit.pages)
-        own_pages = self.current_pages.keys()
-        if len(adv_pages) >= len(own_pages):
-            self.suppress_adv()
-
-        missing_pages = adv_pages.difference(own_pages)
-        if len(missing_pages) == 0:
-            return
-
-        # Send REQ for the lowest number page/packet we are missing.
-        # If currently buffering pages, request for missing packets in these pages.
-        if len(self.buffering_pages) != 0:
-            incomplete_page = min(self.buffering_pages.keys())
-            all_packets_in_page = set(xrange(self.packets_per_page[incomplete_page]))
-            received_packets = self.buffering_pages[incomplete_page].keys()
-            missing_packets = all_packets_in_page.difference(received_packets)
-            self.send_req(incomplete_page, missing_packets)
-        else:
-            missing_page = min(missing_pages)
-            self.send_req(
-                missing_page, list(range(self.packets_per_page[missing_page])))
-
-    def process_req(self, data_unit):
-        # Ignore REQ is we don't have the page.
-        requested_page = data_unit.page_number
-        if requested_page not in self.current_pages:
-            return
-        # Send out missing packets
-        for packet_number in data_unit.packets:
-            self.requested.add((requested_page, packet_number))
-
-        self.send_requested()
-
-    def process_data(self, data_unit):
-        # Ignore DATA if wrong version.
-        if self.current_version != data_unit.version:
-            return
-
-        # Ignore DATA is we already have the page
-        page = data_unit.page_number
-        if page in self.current_pages:
-            return
-
-        if page not in self.buffering_pages:
-            self.buffering_pages[page] = {}
-
-        # Ignore if we already have the packet.
-        packet = data_unit.packet_number
-        if packet in self.buffering_pages[page]:
-            return
-
-        self.buffering_pages[page][packet] = data_unit.message
-        # If fullpage, move buffered page into current pages.
-        if len(self.buffering_pages[page]) == self.packets_per_page[page]:
-            self.current_pages[page] = self.buffering_pages[page]
-            del self.buffering_pages[page]
-
-        if len(self.current_pages) == len(self.packets_per_page):
-            self.logger.debug("(%s, %s): Done." % (self.addr, self.get_port()))
+    def get_page(self):
+        data = []
+        for page_no in xrange(len(self.packets_per_page)):
+            page = self.complete_pages[page_no]
+            data.extend(page[i] for i in xrange(self.packets_per_page[page_no]))
+        return "".join(data)
