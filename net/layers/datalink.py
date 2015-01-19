c6e88879d0c320721d3311281d5677ef4700a058
Michael Yong <wrong92@gmail.com>
2015-01-19 16:11:47 +0800

    Fix DataLink forwarding.

--- net/layers/datalink.py
+++ net/layers/datalink.py
@@ -6,9 +6,10 @@ class DataLinkPDU(object):
     # source_addr: H
     # dest_addr: H
     # message_id: B
+    # ttl: B
     # total_size: I
     # piece_number: I
-    HEADER_FORMAT = "HHBII"
+    HEADER_FORMAT = "HHBBII"
     # https://docs.python.org/2/library/struct.html
     # B: unsigned char, 1 byte.
     # H: unsigned short, 2 bytes.
@@ -16,24 +17,28 @@ class DataLinkPDU(object):
     HEADER_SIZE = struct.calcsize(HEADER_FORMAT)
     MAX_DATA_SIZE = 100 - HEADER_SIZE
 
-    def __init__(self, source_addr, dest_addr, message_id, total_size, piece_no,
-            chunk):
+    def __init__(self, source_addr, dest_addr, message_id, ttl, total_size,
+            piece_no, chunk):
         self.source_addr = source_addr
         self.dest_addr = dest_addr
         self.message_id = message_id
         self.total_size = total_size
         self.piece_no = piece_no
         self.chunk = chunk
+        self.ttl = ttl
 
     def to_string(self):
-        header = struct.pack(self.HEADER_FORMAT, self.source_addr,
-            self.dest_addr, self.message_id, self.total_size, self.piece_no)
+        header = struct.pack(
+            self.HEADER_FORMAT,
+            self.source_addr, self.dest_addr,
+            self.message_id, self.ttl,
+            self.total_size, self.piece_no)
         return header + self.chunk
 
     @classmethod
     def from_string(cls, data):
         x = struct.unpack(cls.HEADER_FORMAT, data[:cls.HEADER_SIZE])
-        return DataLinkPDU(x[0], x[1], x[2], x[3], x[4], data[cls.HEADER_SIZE:])
+        return DataLinkPDU(x[0], x[1], x[2], x[3], x[4], x[5], data[cls.HEADER_SIZE:])
 
 
 class DataLink(base.BaseLayer):
@@ -42,16 +47,22 @@ class DataLink(base.BaseLayer):
     - Converts variable sized data into fixed sized packets.
     - Forwards packets along the network.
     """
-    def __init__(self):
+    def __init__(self, ttl=0, buffer_window=10):
         super(DataLink, self).__init__()
         self.last_message_id = 0
+
+        # Maximum number of hops to forward a message.
+        self.ttl = ttl
+
+        # Buffer of pieces: buffer[source_addr][message_id][piece_no]
         self.buffer = {}
-        self.seen_buffer = set()
-        self.forward_buffer = set()
+
+        # Number of previous messages to keep around.
+        self.buffer_window = buffer_window
 
     def get_next_message_id(self):
         # restrict message id to be from 1 - 255
-        self.last_message_id = (self.last_message_id + 1) % 255
+        self.last_message_id = (self.last_message_id % 255) + 1
         return self.last_message_id
 
     def _chunk_data(self, data):
@@ -74,9 +85,27 @@ class DataLink(base.BaseLayer):
             dest_addr = metadata.dest_addr
 
         for piece_no, chunk in enumerate(self._chunk_data(data)):
-            data_unit = DataLinkPDU(self.addr, dest_addr, message_id,
+            data_unit = DataLinkPDU(
+                self.addr, dest_addr, message_id, self.ttl,
                 total_size, piece_no, chunk)
             self.put_outgoing(data_unit.to_string(), metadata)
+
+    def process_incoming(self, data, metadata=None):
+        data_unit = DataLinkPDU.from_string(data)
+
+        # Ignore packet if we've already received it.
+        sender = data_unit.source_addr
+        m_id = data_unit.message_id
+        p_no = data_unit.piece_no
+        if sender not in self.buffer:
+            self.buffer[sender] = {}
+        if m_id not in self.buffer[sender]:
+            self.buffer[sender][m_id] = {}
+        if p_no in self.buffer[sender][m_id]:
+            return
+
+        self._maybe_forward_data(data_unit)
+        self._maybe_buffer_incoming(data_unit, metadata)
 
     def _maybe_forward_data(self, data_unit):
         if data_unit.dest_addr == base.MetaData.BROADCAST_ADDR:
@@ -84,14 +113,12 @@ class DataLink(base.BaseLayer):
         # Do not forward if we are the intended recipient.
         if data_unit.dest_addr == self.addr:
             return
-        # Ignore packet if we've already received it.
-        key = (data_unit.source_addr, data_unit.message_id,
-            data_unit.piece_no)
-        if key in self.forward_buffer:
+        # Do not forward if ttl is less than 1.
+        if data_unit.ttl <= 0:
             return
-        # Forward packet if we've not seen this.
-        # TODO: Add TTL and decrement?
-        self.forward_buffer.add(key)
+
+        # Forward packet.
+        data_unit.ttl -= 1
         self.put_outgoing(data_unit.to_string())
 
     def _maybe_buffer_incoming(self, data_unit, metadata=None):
@@ -100,29 +127,48 @@ class DataLink(base.BaseLayer):
             data_unit.dest_addr != self.addr:
             return
 
-        # Logging to see the packet loss rate.
-        # self.logger.debug("Incoming: %s, %s, %s" % \
-        #     (data_unit.source_addr, data_unit.message_id, data_unit.piece_no))
+        sender = data_unit.source_addr
+        m_id = data_unit.message_id
+        p_no = data_unit.piece_no
+        self.buffer[sender][m_id][p_no] = data_unit.chunk
+        buffered_chunks = self.buffer[sender][m_id]
 
-        key = (data_unit.source_addr, data_unit.message_id)
-        if key in self.seen_buffer:
-            return
-        buffered_chunks = self.buffer.get(key, {})
-        buffered_chunks[data_unit.piece_no] = data_unit.chunk
-        self.buffer[key] = buffered_chunks
         size = sum((len(x) for x in buffered_chunks.values()))
         if size == data_unit.total_size:
-            del self.buffer[key]
-            self.seen_buffer.add(key)
             keys = sorted(buffered_chunks.keys())
             data = "".join(buffered_chunks[x] for x in keys)
-            # propagate metadata 
+            # propagate metadata
             metadata = metadata or base.MetaData()
             metadata.source_addr = data_unit.source_addr
             self.put_incoming(data, metadata)
 
-    def process_incoming(self, data, metadata=None):
-        data_unit = DataLinkPDU.from_string(data)
-        # TODO: Add checksum/verify step.
-        self._maybe_forward_data(data_unit)
-        self._maybe_buffer_incoming(data_unit, metadata)
+            # clear buffer
+            self._clear_buffer(sender, m_id)
+
+    def _clear_buffer(self, sender, message_id):
+        # Remove all messages older than (sender, message_id - 10)
+        min_m_id = (message_id - self.buffer_window) % 255
+        for m_id in self.buffer[sender].keys():
+            if min_m_id <= m_id <= message_id:
+                continue
+            del self.buffer[sender][m_id]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- net/layers/test_datalink.py
+++ net/layers/test_datalink.py
@@ -19,23 +19,25 @@ class TestDataLink(object):
 
     def test_pdu_constructor(self):
         data_unit = datalink.DataLinkPDU(self.addr, self.dest_addr,
-            self.message_id, 100, 1, self.short_data)
+            self.message_id, 2, 100, 1, self.short_data)
         eq_(self.addr, data_unit.source_addr)
         eq_(self.dest_addr, data_unit.dest_addr)
         eq_(self.message_id, data_unit.message_id)
         eq_(100, data_unit.total_size)
         eq_(1, data_unit.piece_no)
+        eq_(2, data_unit.ttl)
         eq_(self.short_data, data_unit.chunk)
 
     def test_pdu_round_trip(self):
         data_unit = datalink.DataLinkPDU(self.addr, self.dest_addr,
-            self.message_id, 100, 1, self.short_data)
+            self.message_id, 2, 100, 1, self.short_data)
         data_unit = datalink.DataLinkPDU.from_string(data_unit.to_string())
         eq_(self.addr, data_unit.source_addr)
         eq_(self.dest_addr, data_unit.dest_addr)
         eq_(self.message_id, data_unit.message_id)
         eq_(100, data_unit.total_size)
         eq_(1, data_unit.piece_no)
+        eq_(2, data_unit.ttl)
         eq_(self.short_data, data_unit.chunk)
 
     def test_get_next_message_id(self):
@@ -66,37 +68,33 @@ class TestDataLink(object):
     def test_should_not_receive_if_not_recipient(self):
         data_unit = datalink.DataLinkPDU(
             self.addr, base.MetaData.BROADCAST_ADDR,
-            self.message_id, 100, 1, self.short_data)
+            self.message_id, 1, 100, 1, self.short_data)
         self.data_link_layer.process_incoming(data_unit.to_string())
         ok_(self.data_link_layer.is_incoming_empty())
 
     def test_should_not_forward_broadcast(self):
         data_unit = datalink.DataLinkPDU(
             self.addr, base.MetaData.BROADCAST_ADDR,
-            self.message_id, 100, 1, self.short_data)
+            self.message_id, 1, 100, 1, self.short_data)
         self.data_link_layer.process_incoming(data_unit.to_string())
         ok_(self.data_link_layer.is_outgoing_empty())
 
     def test_should_not_forward_if_recipient(self):
         data_unit = datalink.DataLinkPDU(
-            self.addr, self.addr, self.message_id, 100, 1, self.short_data)
+            self.addr, self.addr, self.message_id, 1, 100, 1, self.short_data)
         self.data_link_layer.process_incoming(data_unit.to_string())
         ok_(self.data_link_layer.is_outgoing_empty())
 
-    def test_should_not_forward_if_buffered(self):
+    def test_should_not_forward_if_ttl_zero(self):
         data_unit = datalink.DataLinkPDU(
-            self.addr, self.addr + 1,
-            self.message_id, 100, 1, self.short_data)
-        self.data_link_layer.process_incoming(data_unit.to_string())
-        ok_(not self.data_link_layer.is_outgoing_empty())
-        chunk, metadata = self.data_link_layer.get_outgoing()
+            self.addr, self.addr, self.message_id, 0, 100, 1, self.short_data)
         self.data_link_layer.process_incoming(data_unit.to_string())
         ok_(self.data_link_layer.is_outgoing_empty())
 
     def test_should_forward(self):
         data_unit = datalink.DataLinkPDU(
             self.addr, self.addr + 1,
-            self.message_id, len(self.short_data), 1, self.short_data)
+            self.message_id, 1, len(self.short_data), 1, self.short_data)
         self.data_link_layer.process_incoming(data_unit.to_string())
         ok_(not self.data_link_layer.is_outgoing_empty())
         chunk, metadata = self.data_link_layer.get_outgoing()
@@ -111,12 +109,39 @@ class TestDataLink(object):
     def test_should_not_forward_if_seen(self):
         data_unit = datalink.DataLinkPDU(
             self.addr, self.addr + 1,
-            self.message_id, len(self.short_data), 1, self.short_data)
+            self.message_id, 1, len(self.short_data), 1, self.short_data)
         self.data_link_layer.process_incoming(data_unit.to_string())
         ok_(not self.data_link_layer.is_outgoing_empty())
         # Clear both queues.
         chunk, metadata = self.data_link_layer.get_outgoing()
+        data_unit.ttl -= 1
         eq_(data_unit.to_string(), chunk)
         # Receive same data unit.
         self.data_link_layer.process_incoming(data_unit.to_string())
         ok_(self.data_link_layer.is_outgoing_empty())
+
+    def test_should_flush_buffer(self):
+        self.data_link_layer.buffer_window = 1
+        data_unit1 = datalink.DataLinkPDU(
+            self.dest_addr, self.addr,
+            self.message_id, 1, len(self.short_data), 1, self.short_data)
+        self.data_link_layer.process_incoming(data_unit1.to_string())
+        # One message buffered from self.dest_addr
+        eq_(1, len(self.data_link_layer.buffer[self.dest_addr]))
+        
+        data_unit2 = datalink.DataLinkPDU(
+            self.dest_addr, self.addr,
+            self.message_id + 1, 1, len(self.short_data), 1, self.short_data)
+        self.data_link_layer.process_incoming(data_unit2.to_string())
+        # two messages buffered from self.dest_addr
+        eq_(2, len(self.data_link_layer.buffer[self.dest_addr]))
+
+        data_unit3 = datalink.DataLinkPDU(
+            self.dest_addr, self.addr,
+            self.message_id + 2, 1, len(self.short_data), 1, self.short_data)
+        self.data_link_layer.process_incoming(data_unit3.to_string())
+        # two messages (the latest 2) buffered from self.dest_addr
+        eq_(2, len(self.data_link_layer.buffer[self.dest_addr]))
+        ok_(self.message_id + 1 in self.data_link_layer.buffer[self.dest_addr])
+        ok_(self.message_id + 2 in self.data_link_layer.buffer[self.dest_addr])
+
