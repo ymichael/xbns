b96f5182061a0c9af5f9e2611b0b7294ac858c5b
Michael Yong <wrong92@gmail.com>
2015-02-02 20:42:53 +0800

    [Deluge/Rateless] Add app/run_*.py files to start protocols.

---
+++ app/run_deluge.py
@@ -0,0 +1,25 @@
+import argparse
+import deluge
+import time
+
+
+def main(args):
+    app = deluge.Deluge.create_and_run_application()
+
+    # Seed data.
+    seed_data = args.file.read()
+    args.file.close()
+    app.new_version(args.version, seed_data)
+
+    while True:
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Deluge Application')
+    parser.add_argument('--file', '-f', type=argparse.FileType(), required=True,
+                        help='File to seed as the initial version of the data.')
+    parser.add_argument('--version', '-v', type=int, default=1,
+                        help='The version number of the seed file.')
+    args = parser.parse_args()
+    main(args)
