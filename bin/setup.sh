d243a9755d13a71fdeeb9a00e3a07578554bcced
Michael Yong <wrong92@gmail.com>
2015-02-21 15:46:13 +0800

    Ensure that setup.sh is run as root.

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
 
 install () {
     # Remove and uninstall the service (if exists).
