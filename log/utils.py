91952faae311821823741f60efc0aca49a863161
Michael Yong <wrong92@gmail.com>
2015-03-16 23:40:45 +0800

    Revert "Log: sort page completion by itself."
    This reverts commit a2fe1fe3aa52111a5ce4c141da74654c038d30c9.

--- log/utils.py
+++ log/utils.py
@@ -223,11 +223,15 @@ def pprint_packets_sent(packets_sent):
 
 
 def pprint_completion_times(ct):
-    # sort nodes by times (page0, page1, ...)
-    total_pages = len(ct[ct.keys()[0]]) - 1
-    def key_func(node):
-        return [ct[node][x] for x in xrange(total_pages + 1)]
-    nodes = sorted(ct.keys(), key=key_func)
+    nodes = ct.keys()
+    total_pages = len(ct[nodes[0]]) - 1
+
+    # sort nodes by start times
+    start_times = {}
+    for n in nodes:
+        if ct[n].get(0):
+            start_times[n] = ct[n][0]
+    nodes = sorted(start_times.keys(), key=lambda n: start_times.get(n))
     seed = nodes[0]
 
     # Order events to better visualize pipelining.
